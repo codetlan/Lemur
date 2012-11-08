9c856d1f9a8ff80d25a30d4b831cb5257aeacbf0
jose armando gonzalez lopez <iam@armando.mx>
2012-11-08 00:54:56 -0600

    integracion de css y js para meteor

---
+++ model.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Armando Gonzalez <iam@armando.mx>
+ *
+ * The models behavior of lemur app
+ * 	-Usuarios
+	-Equipos
+	-Partidos o fechas
+	-Resultados
+	-Puntajes
+	-Premios
+	-Torneo
+	-Pagos
+ */
+
+//Equipos
+
+Clubs = new Meteor.Collection("clubs");
+Matches = new Meteor.Collection("matches");
+Prices = new Meteor.Collection("Prices");
